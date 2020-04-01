e718d492917316bdca350e696adc4d6337fd228f
Mustafa TUNCER <tuncerm@gmail.com>
2020-04-01 14:35:21 +0200

    Testing class vs struct

--- main.cpp
+++ main.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
+#include <cassert>
 
-struct Date {
-    public:
-        int Day(){ return day; } // getter -- Accessor function
-        void Day(int d) {day = d;} //setter -- Mutator function
+class Date {
+public:
+    int Day() { return day; }
+    void Day(int d) {
+        if (d >= 1 && d <= 31) day = d;
+    }
+    int Month() { return month; }
+    void Month(int m) {
+        if(m >= 1 && m <= 12)
+            month = m;
+    }
+    int Year() { return year; }
+    void Year(int y) {
+        year = y;
+    }
+
+private:
+    int day{1};
     int month{1};
-    int year{1970};
-    private:
-        int day{1}; // Private member
+    int year{0};
 };
 
-int main() {
+// Test in main
+int main()
+{
     Date date;
-
-    date.Day(29);
-
-    std::cout << date.Day() << "/" << date.month << "/" << date.year << std::endl;
-    return 0;
+    date.Day(-1);
+    date.Month(14);
+    date.Year(2000);
+    assert(date.Day() != -1);
+    assert(date.Month() != 14);
+    assert(date.Year() == 2000);
+    std::cout << date.Day() << "/" << date.Month() << "/" << date.Year() << "\n";
 }
