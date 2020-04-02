f3176321435074eed70588a57836b7f6f5449ee1
Mustafa TUNCER <tuncerm@gmail.com>
2020-04-02 14:20:32 +0200

    Getters and setters

--- main.cpp
+++ main.cpp
@@ -2,14 +2,48 @@
 #include <iostream>
 #include <string>
 
+class BankAccount {
+
+private:
+    std::string account_number;
+    std::string owner_name;
+    double available_funds;
+
+public:
+    void PrintInfo();
+
+    BankAccount(std::string account_number,
+                std::string owner_name,
+                double available_funds) :
+            account_number(account_number),
+            owner_name(owner_name),
+            available_funds(available_funds) {};
+    // TODO: declare getters
+};
+
+// TODO: implement setters
+void BankAccount::PrintInfo() {
+    std::cout << "_______INFO_______\n";
+    std::cout << "ID: " << account_number << "\n";
+    std::cout << "Name: " << owner_name << "\n";
+    std::cout << "Funds: $" << available_funds << "\n";
+    std::cout << "_______ENDINFO____\n";
+}
+
 class Date {
 public:
     Date(int d, int m, int y);
+
     int Day() { return day; }
+
     void Day(int d);
+
     int Month() { return month; }
+
     void Month(int m);
+
     int Year() { return year; }
+
     void Year(int y);
 
 private:
@@ -24,12 +58,12 @@ Date::Date(int day, int month, int year) : year(year) {
 }
 
 void Date::Day(int day) {
-    if(day >= 1 && day <= 31)
+    if (day >= 1 && day <= 31)
         Date::day = day;
 }
 
 void Date::Month(int month) {
-    if(month >= 1 && month <= 12)
+    if (month >= 1 && month <= 12)
         Date::month = month;
 }
 
@@ -40,10 +74,10 @@ void Date::Year(int year) { Date::year = year; }
 //        Person(std::string n): name(n){};
 //        std::string name;
 //};
-class Person{
-    public:
-        Person(std::string const &n): name(n){}; // if used to initialize, the reference should also be a const.
-        std::string const name;
+class Person {
+public:
+    Person(std::string const &n) : name(n) {}; // if used to initialize, the reference should also be a const.
+    std::string const name;
 };
 
 // Can also use struct here.
@@ -53,27 +87,31 @@ class Person{
 //};
 
 class Birthday {
-    public:
-        Birthday(int day, int month, int year) : day(day), month(month), year(year){}
-        void Print(){
-            std::cout << day << "/" << month << "/" << year << "\n";
-        }
-    private:
-        int const day;
-        int const month;
-        int const year;
+public:
+    Birthday(int day, int month, int year) : day(day), month(month), year(year) {}
+
+    void Print() {
+        std::cout << day << "/" << month << "/" << year << "\n";
+    }
+
+private:
+    int const day;
+    int const month;
+    int const year;
 };
 
 // Test in main
-int main()
-{
+int main() {
     Person Ali = Person("Ali");
     std::cout << Ali.name << "\n";
-    Date date(8,29,1981);
+    Date date(8, 29, 1981);
     Birthday bday(8, 7, 1986);
     assert(date.Day() == 8);
     assert(date.Month() != 29);
     assert(date.Year() == 1981);
     std::cout << date.Day() << "/" << date.Month() << "/" << date.Year() << "\n";
     bday.Print();
+
+    BankAccount account("12312313", "Pikachu", 99.99);
+    account.PrintInfo();
 }
