db63c77d8f45c862aa66baf72d04b212259a15e5
Mustafa TUNCER <tuncerm@gmail.com>
2020-04-03 13:32:09 +0200

    Inheritance 2, public, protected & private

---
+++ inheritance2.cpp
@@ -0,0 +1,69 @@
+// This example demonstrates the privacy levels
+// between parent and child classes
+#include <iostream>
+#include <string>
+using std::string;
+
+class Vehicle {
+public:
+    int wheels = 0;
+    string color = "blue";
+
+    void Print() const
+    {
+        std::cout << "This " << color << " vehicle has " << wheels << " wheels!\n";
+    }
+};
+
+class Car : public Vehicle {
+public:
+    bool sunroof = false;
+};
+
+class Bicycle : protected Vehicle {
+public:
+    bool kickstand = true;
+    void Wheels(int w)
+    {
+        wheels = w;
+    }
+};
+
+class ElectricBicycle: public Bicycle{
+public:
+    void Wheels(int w)
+    {
+        wheels = w;
+    }
+};
+
+class Scooter : private Vehicle {   // private
+public:
+    bool electric = false;
+    void Wheels(int w)
+    {
+        wheels = w;
+    }
+};
+
+class ElectricScooter: public Scooter{
+public:
+    void Wheels(int w)
+    {
+        wheels = w;  // Error error: cannot cast 'ElectricScooter' to its private base class 'Vehicle' wheels = w;
+    }
+};
+
+int main()
+{
+    Car car;
+    car.wheels = 4;
+    Bicycle bicycle;
+    bicycle.Wheels(2);
+    Scooter scooter;
+    scooter.Wheels(1);
+    ElectricBicycle ebike;
+    ebike.Wheels(3);
+    ElectricScooter escooter;
+    escooter.Wheels(5);
+};
