0b9c5e6c734df6b23f6a6d02e3bbc02a5176f154
Mustafa TUNCER <tuncerm@gmail.com>
2020-04-16 01:02:30 +0200

    Creating a message queue-solution.

---
+++ concurrent-message-queue-solution.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <thread>
+#include <queue>
+#include <future>
+#include <mutex>
+#include <algorithm>
+
+template<class Type>
+class MessageQueue
+{
+public:
+    MessageQueue() {}
+
+    Type receive()
+    {
+        // perform vector modification under the lock
+        std::unique_lock<std::mutex> uLock(_mutex);
+        _cond.wait(uLock, [this] { return !_messages.empty(); }); // pass unique lock to condition variable
+
+        // remove last vector element from queue
+        Type m = std::move(_messages.back());
+        _messages.pop_back();
+
+        return m; // will not be copied due to return value optimization (RVO) in C++
+    }
+
+    void send(Type &&m)
+    {
+        // simulate some work
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        // perform vector modification under the lock
+        std::lock_guard<std::mutex> uLock(_mutex);
+
+        // add vector to queue
+        std::cout << "   Message #" << m << " will be added to the queue" << std::endl;
+        _messages.push_back(std::move(m));
+        _cond.notify_one(); // notify client after pushing new Vehicle into vector
+    }
+
+private:
+    std::mutex _mutex;
+    std::condition_variable _cond;
+    std::deque<Type> _messages; // list of all vehicles waiting to enter this intersection
+};
+
+int main()
+{
+    // create monitor object as a shared pointer to enable access by multiple threads
+    std::shared_ptr<MessageQueue<int>> queue(new MessageQueue<int>);
+
+    std::cout << "Spawning threads..." << std::endl;
+    std::vector<std::future<void>> futures;
+    for (int i = 0; i < 10; ++i)
+    {
+        int message = i;
+        futures.emplace_back(std::async(std::launch::async, &MessageQueue<int>::send, queue, std::move(message)));
+    }
+
+    std::cout << "Collecting results..." << std::endl;
+    while (true)
+    {
+        int message = queue->receive();
+        std::cout << "   Message #" << message << " has been removed from the queue" << std::endl;
+    }
+
+    std::for_each(futures.begin(), futures.end(), [](std::future<void> &ftr) {
+        ftr.wait();
+    });
+
+    std::cout << "Finished!" << std::endl;
+
+    return 0;
+}
