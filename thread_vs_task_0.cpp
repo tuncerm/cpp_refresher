d3fd6b816b07b8f8fa96c5c63d70a470acff5051
Mustafa TUNCER <tuncerm@gmail.com>
2020-04-15 12:26:12 +0200

    Converting a thread to task

---
+++ thread_vs_task_0.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <thread>
+#include <future>
+#include <cmath>
+#include <memory>
+//void divideByNumber(std::promise<double> &&prms, double num, double denom)
+double divideByNumber(double num, double denom)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(500)); // simulate work
+//    try
+//    {
+    if (denom == 0)
+        throw std::runtime_error("Exception from thread: Division by zero!");
+
+    return num / denom;
+//    else
+//    prms.set_value(num / denom);
+//}
+//catch (...)
+//{
+//    prms.set_exception(std::current_exception());
+//}
+}
+
+
+int main()
+{
+//    // create promise and future
+//    std::promise<double> prms;
+//    std::future<double> ftr = prms.get_future();
+
+    // use async to start a task
+    double num = 42.0, denom = 2.0;
+//    std::thread t(divideByNumber, std::move(prms), num, denom);
+    std::future<double> ftr = std::async(divideByNumber, num, denom);
+
+    // retrieve result within try-catch-block
+    try
+    {
+        double result = ftr.get();
+        std::cout << "Result = " << result << std::endl;
+    }
+    catch (std::runtime_error e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+//    // thread barrier
+//    t.join();
+
+    return 0;
+}
