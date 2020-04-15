14e3db4b4ec209fe974134b760b5ef3d51e605bc
Mustafa TUNCER <tuncerm@gmail.com>
2020-04-15 21:47:28 +0200

    Mutex - Mutual exclusion.

---
+++ mutex.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <future>
+#include <mutex>
+#include<algorithm>
+
+class Vehicle
+{
+public:
+    Vehicle(int id) : _id(id) {}
+
+private:
+    int _id;
+};
+
+class WaitingVehicles
+{
+public:
+    WaitingVehicles() {}
+
+    // getters / setters
+    void printSize()
+    {
+        _mutex.lock();
+        std::cout << "#vehicles = " << _vehicles.size() << std::endl;
+        _mutex.unlock();
+    }
+
+    // typical behaviour methods
+    void pushBack(Vehicle &&v)
+    {
+        for(int i = 0; i < 3; ++i){
+            if(_mutex.try_lock_for(std::chrono::milliseconds(100))){
+                // data race would cause an exception
+                _vehicles.emplace_back(std::move(v));
+                _mutex.unlock();
+                break;
+            }
+        }
+    }
+
+private:
+    std::vector<Vehicle> _vehicles; // list of all vehicles waiting to enter this intersection
+    std::timed_mutex _mutex;
+};
+
+int main()
+{
+    std::shared_ptr<WaitingVehicles> queue(new WaitingVehicles);
+    std::vector<std::future<void>> futures;
+    for (int i = 0; i < 1000; ++i)
+    {
+        Vehicle v(i);
+        futures.emplace_back(std::async(std::launch::async, &WaitingVehicles::pushBack, queue, std::move(v)));
+    }
+
+    std::for_each(futures.begin(), futures.end(), [](std::future<void> &ftr) {
+        ftr.wait();
+    });
+
+    queue->printSize();
+
+    return 0;
+}
